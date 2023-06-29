aed1ddec4b5433c771a1be009389fe3257666076
Abhijeet Kumar <akc972527@gmail.com>
2023-06-30 01:35:48 +0530

    exit chat

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
 import { MdCall } from "react-icons/md";
 import { IoVideocam } from "react-icons/io5";
@@ -6,9 +6,31 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import ContextMenu from "../common/ContextMenu";
 
 function ChatHeader() {
   const [{ currentChatUser }, dispatch] = useStateProvider();
+
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCoordinates({ x: e.pageX - 50, y: e.pageY + 20 });
+    setIsContextMenuVisible(true);
+  }
+
+  const contextMenuOptions = [
+    {
+      name: "Exit",
+      callback: async () => {
+        dispatch({ type: reducerCases.SET_EXIT_CHAT });
+      },
+    },
+  ];
 
   const handleVoiceCall = () => {
     dispatch({
@@ -51,7 +73,10 @@ function ChatHeader() {
         <MdCall onClick={handleVoiceCall} className="text-xl cursor-pointer text-panel-header-icon" />
         <IoVideocam onClick={handleVideoCall} className="text-xl cursor-pointer text-panel-header-icon" />
         <BiSearchAlt2 className="text-xl cursor-pointer text-panel-header-icon" onClick={() => dispatch({ type: reducerCases.SET_MESSAGE_SEARCH })} />
-        <BsThreeDotsVertical className="text-xl cursor-pointer text-panel-header-icon" />
+        <BsThreeDotsVertical onClick={(e) => showContextMenu(e)} id="context-opener" className="text-xl cursor-pointer text-panel-header-icon" />
+        {isContextMenuVisible && (
+          <ContextMenu options={contextMenuOptions} coordinates={contextMenuCoordinates} ContextMenu={isContextMenuVisible} setContextMenu={setIsContextMenuVisible} />
+        )}
       </div>
     </div>
   );
